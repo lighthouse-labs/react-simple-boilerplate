f22422b007b1fd8b7cc249db1a3b466e8a04c99c
vaz <v@tryptid.com>
2017-05-19 15:03:14 -0700

    try both

--- react/server.js
+++ react/server.js
@@ -2,6 +2,8 @@ var webpack = require('webpack');
 var WebpackDevServer = require('webpack-dev-server');
 var config = require('./webpack.config');
 
+const PORT = process.env.PORT || 3001
+
 new WebpackDevServer(webpack(config), {
     publicPath: config.output.publicPath,
     watchOptions: {
@@ -10,10 +12,10 @@ new WebpackDevServer(webpack(config), {
       ignored: /node_modules/
     }
   })
-  .listen(3000, '0.0.0.0', function (err, result) {
+  .listen(PORT, '0.0.0.0', function (err, result) {
     if (err) {
       console.log(err);
     }
 
-    console.log('Running at http://0.0.0.0:3000');
+    console.log('Running on port ', PORT);
   });
